57417f7d2350330f87f472db8aff557253a7d5b7
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-12-03 13:50:25 -0500

    Merge Master and Digitalread()

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -7,10 +7,16 @@
  */
 //initialize our led to Pin 13
 int led = 13;
+//digital pin2 has a push button attached to it
+int pushButton = 2;
 
 void setup() {
   // Using the Function pinMode to call the variable led and provide Output
   pinMode(led, OUTPUT);
+   // initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+ // make the pushButton pin an intput
+ pinMode(pushButton, INPUT); 
 }
 
 void loop() {
@@ -18,5 +24,10 @@ void loop() {
   digitalWrite(led, HIGH);
   delay(1500);
   digitalWrite(led, LOW);
-  delay(500);  
+  delay(500);
+   // read the input pin
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button
+  Serial.println(buttonState);
+  delay(250); //delay in between readings for stability  
 }
