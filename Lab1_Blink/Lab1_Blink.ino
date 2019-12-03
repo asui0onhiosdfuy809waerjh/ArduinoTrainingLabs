eb08f3c883d739ccc775ef1ac4bb9081b7876577
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-12-03 14:01:37 -0500

    Add if-else to have LED go on and off based off of a button state

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,33 +1,45 @@
 /* Programmer: Tyler Brunner
- *  Date: 11.6.19
- *  Program: Blink
- *  
- * This program will turn on an LED for specified amounts of time, turn it
- * off, and back on in a While Loop.
- */
-//initialize our led to Pin 13
+   Date: 11.6.19
+   Program: Blink
+
+   This program will turn on an LED for specified amounts of time, turn it off, and back on in a While Loop
+
+   Merged on 12.2.19
+
+   Programmer: Tyler Brunner
+   Date: 11.6.19
+   Program: digitalRead & the Serial Port
+
+   Reads a digital input on pin2, prints
+*/
+
+
+// itialize our led to Pin 13
 int led = 13;
-//digital pin2 has a push button attached to it
+
+//digital pin 2 has a push button attached to it
 int pushButton = 2;
 
 void setup() {
-  // Using the Function pinMode to call the variable led and provide Output
+  // Using the Function pinMode to call the vairable led and provide OUTPUT
   pinMode(led, OUTPUT);
-   // initializes serial communication at 9600 bits per second
+  //initializes serial communication at 9600 bits per second
   Serial.begin(9600);
- // make the pushButton pin an intput
- pinMode(pushButton, INPUT); 
+  //make the pushButton pin an input
+  pinMode(pushButton, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led, HIGH);
-  delay(1500);
-  digitalWrite(led, LOW);
-  delay(500);
-   // read the input pin
+  // read the input pin
   int buttonState = digitalRead(pushButton);
-  // print out the state of the button
+  //print out the state of the button
   Serial.println(buttonState);
-  delay(250); //delay in between readings for stability  
+  // if the buttonState is True/Pushed by user, the LED will blink
+  if (buttonState == HIGH) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
+  delay(1); //delay in between readings for stability
 }
