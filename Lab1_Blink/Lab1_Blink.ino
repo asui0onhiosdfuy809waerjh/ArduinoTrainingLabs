f6bd367c9c735cdbba08cd2310205168819bb186
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-12-11 14:23:52 -0500

    Add Voltage Threshold to turn on LED

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,24 +1,25 @@
 /* Programmer: Tyler Brunner
-   Date: 12.3.19
-   Update: 12.10.19
-   Program: Potentiometer
+   Date: 12.10.19
+   Program: LED&Potentiometer
 
-   AnalogReadSerial
-   Reads an analog input on pin A0, prints to the serial monitor
-
-
-   Add serial read to be able to read voltage from 0 to 5 volts
+   Read analog port and turn on LED at a determined threshold
 */
 
 
+const int analogPin = A0;
+const int ledPin = 13;
+float threshold = 2.25;
+
 void setup() {
+  //initialize the LED pin as an output
+  pinMode(ledPin, OUTPUT);
   // initialize the serial communication at 9600 bits per second
   Serial.begin(9600);
 }
 
 void loop() {
   // read the input on analog pin 0
-  int potentiometerValue = analogRead(A0);
+  int potentiometerValue = analogRead(analogPin);
 
   //Convert the analog read from 0 to 1023 to 0 - 5 volts
   float voltage = potentiometerValue * (5.0 / 1023);
@@ -26,6 +27,13 @@ void loop() {
   // print the value of our potentiometer
   Serial.println(voltage);
 
+  // if the analog value is higher than Threshold Variable it will turn on the LED
+  if (voltage > threshold){
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
   // add a delay for stability
   delay(1);
 
