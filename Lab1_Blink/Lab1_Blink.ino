a35b937d9e013b1cf61cf1aaac40c436528388e0
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-12-11 14:12:01 -0500

    Add voltage read

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -1,9 +1,13 @@
 /* Programmer: Tyler Brunner
    Date: 12.3.19
+   Update: 12.10.19
    Program: Potentiometer
 
    AnalogReadSerial
    Reads an analog input on pin A0, prints to the serial monitor
+
+
+   Add serial read to be able to read voltage from 0 to 5 volts
 */
 
 
@@ -16,8 +20,11 @@ void loop() {
   // read the input on analog pin 0
   int potentiometerValue = analogRead(A0);
 
+  //Convert the analog read from 0 to 1023 to 0 - 5 volts
+  float voltage = potentiometerValue * (5.0 / 1023);
+  
   // print the value of our potentiometer
-  Serial.println(potentiometerValue);
+  Serial.println(voltage);
 
   // add a delay for stability
   delay(1);
